0d8743921951cdd35cfee4d27eaf8a95f94562c4
kasyan~ <iikasyan@gmail.com>
2023-01-23 23:54:46 +0300

    zadacha 1

--- Sharp_1/1/Program.cs
+++ Sharp_1/1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число:");
+int a=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int b=int.Parse(Console.ReadLine()!);
+if (a>b)
+{
+    Console.WriteLine($"{a} больше {b}");
+}
+else if (a<b)
+{
+    Console.WriteLine($"{a} меньше {b}");
+    }
+else
+{
+     Console.WriteLine("Ошибка. Введеные числа равны");
+}  
